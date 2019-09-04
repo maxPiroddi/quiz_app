69c32610619f08b496c26931b53dbac975a2df6a
maxPiroddi <mpiroddi@outlook.com>
2019-09-04 14:11:28 +1000

    Day 2 morning, added more functionality bar JSON implementation.

--- index.rb
+++ index.rb
@@ -2,7 +2,7 @@
 #   Initialize required files
 file = File.open("questions.txt")
 file = File.open("results.json")
-results_file = File.open("results.json")
+results_file = File.read("results.json")
 questions_file = File.read("questions.txt")
 require 'json'
 #==============================================================================================================================#
@@ -25,6 +25,8 @@ class Question
 end
 
 #==============================================================================================================================#
+#   Temporary storage of questions until questins file is functioning
+
 p1 = "What type of animal is Bambi? \n(a) Bear \n(b) Deer \n(c) Bird"
 p2 = "Which country features a maple leaf on its flag? \n(a) Canada \n(b) Norway \n(c) Pakistan"
 p3 = "What game features the terms love, deuce, match and volley? \n(a) Soccer \n(b) Tennis \n(c) Gold"
@@ -53,7 +55,6 @@ questions = [
 
 #==============================================================================================================================#
 #   Introductions
-reply = ""
 #==============================================================================================================================#
 #   Bring JSON into an array, push the new data on the end, and then submit back 
 
@@ -62,9 +63,9 @@ reply = ""
 while true
     system "clear"
     puts
-    puts "●▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬●"
-    puts "Welcome to QuikQuiz!"     #Decorations & Introductions
-    puts "●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●"
+    puts "●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬▬●"
+    puts "●▬Welcome to QuikQuiz!▬●"     #Decorations & Introductions
+    puts "●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●"
     puts "Are you a student or teacher?"
     entry = gets.chomp.downcase
 
@@ -78,9 +79,10 @@ while true
             while counter < 11
                 system "clear"
                 puts
-                puts "●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●"
+                puts "●▬▬▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬▬▬●"
+                puts "●▬▬▬Welcome to the Quiz!▬▬●"
+                puts "●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●"
                 puts "Please enter your name:"
-                puts "●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●"
     
                 student_name = gets.chomp.downcase
 
@@ -89,6 +91,7 @@ while true
                     puts
                     puts "Invalid selection: Please enter a name."    # TO DO --> MAKE THIS LOOP BACK TO NAME ENTRY
                     next
+                
                 else
                     for question in questions
                         system "clear"
@@ -103,13 +106,21 @@ while true
                 end
                 system "clear"
                     if score == questions.length()
+                        puts
                         puts "Congratulations! You have a perfect score."
+                        puts
                     elsif score >= (questions.length() * 0.75)
+                        puts
                         puts "Well done! You received #{score}/#{questions.length()}."
+                        puts
                     elsif score >= (questions.length() * 0.5)
+                        puts
                         puts "You received #{score}/#{questions.length()}."
+                        puts
                     else
+                        puts
                         puts "You have failed the quiz, with a score of #{score}/#{questions.length}." 
+                        puts
                     end
                     # File.open("results.json", "a+") do |line|
                     #     line.print "{   \"name\": " + "\"#{student_name.to_s}\"," 
@@ -118,15 +129,15 @@ while true
                     #     line.print "]"
                     # end
 
-                    data_hash=JSON.parse(results_file)
-                        working_array = []
-                        data_hash = working_array  ##FIGURE OUT HOW TO MAKE IT SO THAT THIS EXTRACTS THE STRING
-                        working_array.push("{   \"name\": \"#{student_name.to_s}\", \"score\": \"#{score.to_s}\", \"date\": \"#{Time.now.strftime("%d-%b-%y")}\"}")
-                        data_hash.close()
-                        working_array.to_json
+                    # data_hash=JSON.parse(results_file)
+                    #     working_array = []
+                    #     data_hash = working_array  ##FIGURE OUT HOW TO MAKE IT SO THAT THIS EXTRACTS THE STRING
+                    #     working_array.push("{   \"name\": \"#{student_name.to_s}\", \"score\": \"#{score.to_s}\", \"date\": \"#{Time.now.strftime("%d-%b-%y")}\"}")
+                    #     data_hash.close()
+                    #     working_array.to_json
 
-                    File.open("results.json", "w")
-                        print working_array
+                    # File.open("results.json", "w")
+                    #     print working_array
 
                     
             end
@@ -143,19 +154,29 @@ while true
         end
 
     elsif entry == "teacher"
-        puts "Welcome teacher!"
         
-        # data_hash=JSON.parse(results_file)
-        
-        # for item in data_hash do
-        #     puts "Name: " + item["name"].capitalize
-        #     puts "Score: " + item["score"] + "/10"
-        #     puts "Date: " + item["date"]
-        #     puts
-        #     puts
-        # end
+        system "clear"
 
-        break
+        puts "Welcome teacher!"
+        puts
+        data_hash=JSON.parse(results_file)
+        
+        for item in data_hash do
+            puts "Name: " + item["name"].capitalize
+            puts "Score: " + item["score"] + "/10"
+            puts "Date: " + item["date"]
+            puts
+            puts
+        end
+        puts
+
+        puts "Enter any key to try again, or type 'exit' to quit!"
+        reply = gets.chomp.downcase
+        if reply == "exit"
+            break                           # CHANGE THIS TO REFLECT THE NEEDS OF THE TEACHER SECTION
+        else
+            next
+        end
     
     else
         puts "Invalid selection: Please enter 'student' or 'teacher'"
@@ -165,3 +186,9 @@ end
 #==============================================================================================================================#
 #           Farewell for now!
 #==============================================================================================================================#
+
+# begin
+#     add()
+# rescue      #==> If line 26 is going to return an error, rescue runs. Only shows if an error between begin & end runs.
+#     p "I don't want this to crash!"
+# end
